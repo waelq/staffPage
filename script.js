9e01619910215b1e738b3ded5965eaf24fa213cd
waelq <waealtaqia20152729@gmail.com>
2020-05-19 17:53:24 +0300

    design

---
+++ script.js
@@ -0,0 +1,19 @@
+
+	
+
+			$("#add").click(
+			console.log($("form#age").val())
+			)
+
+		
+/*
+var add = $("#add")	
+var display =$("#Display")	
+
+var item =[]
+add.click(function () {
+	for(var key in info){
+		item.push(info[key])
+	}
+	console.log(item)
+})*/
